241c72ad98e4eaa3152bdb29e242d81d156f4f6d
Lakshika Paiva <41281079+lakshikap@users.noreply.github.com>
2025-03-03 19:41:18 -0500

    Delete basic-exercises directory

--- basic-exercises/2 Retrieve Data using Numeric Query.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM movies ORDER BY release_year DESC;
-SELECT * FROM movies WHERE release_year=2022;
-SELECT * FROM movies WHERE release_year>=2020;
-SELECT * FROM movies WHERE release_year>=2020 AND imdb_rating>8;
-SELECT * FROM movies WHERE studio IN ("Marvel Studios", "Hombale Films");
-SELECT release_year FROM movies WHERE title LIKE "%THOR%";
-SELECT * FROM movies WHERE studio <> "Marvel Studios";
-SELECT title FROM movies WHERE studio NOT IN ("Marvel Studios");
